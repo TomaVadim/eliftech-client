54330bad62a85f93bbc26270920664861dc3b2d8
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-05-16 14:16:16 +0300

    chore: update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import Link from "next/link";
+
+import { Box, Button, Container, Typography } from "@mui/material";
+
+import { PUBLIC_ROUTES } from "@/shared/enums/routes/public-routes";
+
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <Container
+      component="section"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        component="article"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "2rem",
+        }}
+      >
+        <Typography variant="h1" component="h1">
+          Events
+        </Typography>
+
+        <Button variant="contained" size="large">
+          <Link href={PUBLIC_ROUTES.EVENTS}>Look at events</Link>
+        </Button>
+      </Box>
+    </Container>
+  );
 }
