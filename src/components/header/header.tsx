bd825a9b508aebbabd1ab0427be1ded307cd00ca
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-05-23 21:07:27 +0300

    chore: change styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,29 +1,45 @@
 import Link from "next/link";
 
-import { Button, Container } from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
 
 import { PUBLIC_ROUTES } from "@/shared/enums/routes/public-routes";
 
 export const Header = (): JSX.Element => {
   return (
     <Container
+      maxWidth="xl"
       sx={{
+        top: "1rem",
+        left: 0,
+        right: 0,
+        borderRadius: "20px",
         display: "flex",
-        paddingBlock: "1rem",
-        gap: "1rem",
+        backdropFilter: "blur(4px)",
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+        padding: "1rem",
         justifyContent: "end",
-        position: "relative",
+        position: "fixed",
         zIndex: 1,
       }}
       component="header"
     >
-      <Button variant="contained" size="large">
-        <Link href={PUBLIC_ROUTES.EVENTS}>Look at events</Link>
-      </Button>
+      <Box
+        sx={{
+          position: "relative",
+          display: "flex",
+          gap: "1rem",
+          alignItems: "center",
+          zIndex: 2,
+        }}
+      >
+        <Button variant="contained" size="small">
+          <Link href={PUBLIC_ROUTES.EVENTS}>Look at events</Link>
+        </Button>
 
-      <Button variant="contained" color="secondary" size="large">
-        <Link href={PUBLIC_ROUTES.CREATE}>Create new event</Link>
-      </Button>
+        <Button variant="contained" color="secondary" size="small">
+          <Link href={PUBLIC_ROUTES.CREATE}>Create new event</Link>
+        </Button>
+      </Box>
     </Container>
   );
 };
