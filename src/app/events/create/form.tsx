0e597e67237927c4d98923b72bc970e9636a2ac8
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-05-23 21:06:46 +0300

    feat: add toast to visualize resulut for user interact

--- src/app/events/create/form.tsx
+++ src/app/events/create/form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Box, Button } from "@mui/material";
+import toast, { Toaster } from "react-hot-toast";
 
 import { fetcher } from "@/api/fetcher";
 import { InputField } from "@/components/input-field/input-field";
@@ -20,11 +21,14 @@ export const CreateNewEventForm = () => {
 
   const onSubmit = async (data: CreateEventFormData) => {
     try {
-      await fetcher.post("/events", data);
+      const response = await fetcher.post("/events", data);
 
+      if (response.status === 201) {
+        toast.success("Event created", { id: "create-event" });
+      }
       reset();
     } catch (error) {
-      console.error(error);
+      toast.error("Event creation failed", { id: "create-event" });
     }
   };
 
@@ -34,6 +38,7 @@ export const CreateNewEventForm = () => {
       onSubmit={handleSubmit(onSubmit)}
       sx={{ display: "flex", flexDirection: "column", gap: 3, padding: 2 }}
     >
+      <Toaster position="top-center" toastOptions={{ id: "create-event" }} />
       <InputField
         label="Title"
         name="title"
